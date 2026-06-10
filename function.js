f1210972b940a2d8c7d78e1c8320bfdce1276e08
joy0789 <johirulislamj62@gmail.comm>
2026-06-10 19:53:06 +0600

    What are function and function syntax

---
+++ function.js
@@ -0,0 +1,17 @@
+// basic function structure
+// function functionName () {
+
+// }
+
+// to get function output
+// functionName();
+
+// describe the function
+function brushTeeth(){
+    console.log("pick up the bruch")
+    console.log("add tooth paste")
+    console.log("Brush your teeth for 2 minute")
+}
+
+// call the function for output
+brushTeeth();
