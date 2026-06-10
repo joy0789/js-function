d4356f3a92a325c4c5117e133ce2ceed7b72dc92
joy0789 <johirulislamj62@gmail.comm>
2026-06-10 20:43:33 +0600

    How Function works and argument vs parameters

---
+++ argument.js
@@ -0,0 +1,17 @@
+function doubleIT(number){
+    let doubled = number*2;
+    console.log(number, doubled)
+}
+
+doubleIT(15);
+console.log('-------------');
+doubleIT(117);
+console.log('-------------');
+
+
+let number = 30
+doubleIT(number);
+console.log('-------------');
+
+let money = 150
+doubleIT(money);
