b28f1c1201f51d64710005359195ee5e57cfb183
joy0789 <johirulislamj62@gmail.comm>
2026-06-11 19:01:26 +0600

    Sum of all numbers in an array using function

---
+++ array-operation.js
@@ -0,0 +1,12 @@
+function sumOfNumber(numbers){
+    let sum = 0;
+    for(number of numbers){
+        console.log(number)
+        sum = sum + number;
+    }
+    return sum;
+}
+
+let numbs = [24,51,62,34]
+let sum = sumOfNumber(numbs);
+console.log("sum of the number is: ", sum);
