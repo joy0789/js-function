20438ee2ed2bb8a18c8543efed0a1bb9ecdc2a98
joy0789 <johirulislamj62@gmail.comm>
2026-06-11 18:33:45 +0600

    conditional return of odd and even

---
+++ conditional-return.js
@@ -0,0 +1,23 @@
+// even function
+// function isOdd(number){
+//     if(number % 2 === 0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// console.log(isOdd(5));
+// console.log(isOdd(50));
+
+// odd function
+function isOdd(number){
+    if(number % 2 === 1){
+        return true;
+    }
+    return false;
+}
+
+console.log(isOdd(5));
+console.log(isOdd(50));
